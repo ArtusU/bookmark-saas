2bdfcd175ea21aadc50a1ce0817f4707eae888cc
Artus U <artusrock@hotmail.com>
2022-10-22 10:10:47 +0100

    settings categories.html

--- apps/dashboard/urls.py
+++ apps/dashboard/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
+from apps.bookmarker.views import categories
 from .views import dashboard
 
 
 urlpatterns = [
-    path('', dashboard, name='dashboard')
+    path('', dashboard, name='dashboard'),
+    path('categories/', categories, name='categories'),
 ]
