5807cf013ab730bb41e66933b391b19b81fdb116
Artus U <artusrock@hotmail.com>
2022-10-22 10:28:23 +0100

    static files configuration

--- bookmark/settings.py
+++ bookmark/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 from decouple import config
 
@@ -115,3 +116,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
