b284b671be72018d0181e60324fa27bc1ad51223
Artus U <artusrock@hotmail.com>
2022-10-25 10:53:43 +0100

    rendering categories in categories.html

--- apps/bookmarker/views.py
+++ apps/bookmarker/views.py
@@ -3,4 +3,8 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def categories(request):
-    return render(request, 'bookmarker/categories.html')
+    categories = request.user.categories.all()
+    context = {
+        categories: categories
+    }
+    return render(request, 'bookmarker/categories.html', context)
