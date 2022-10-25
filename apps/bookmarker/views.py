524c2b0715dc2cc36090a76c78a1930ce46f6a04
Artus U <artusrock@hotmail.com>
2022-10-25 12:43:47 +0100

    category detail

--- apps/bookmarker/views.py
+++ apps/bookmarker/views.py
@@ -1,10 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.contrib.auth.decorators import login_required
+
+from .models import Category
 
 @login_required
 def categories(request):
     categories = request.user.categories.all()
     context = {
-        categories: categories
+        'categories': categories
     }
     return render(request, 'bookmarker/categories.html', context)
+
+ 
+@login_required
+def category(request, category_id):
+    category = get_object_or_404(Category, pk=category_id)
+    context = {
+        'category': category
+    }
+    return render(request, 'bookmarker/category.html', context)
--- apps/dashboard/urls.py
+++ apps/dashboard/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from apps.bookmarker.views import categories
+from apps.bookmarker.views import categories, category
 from .views import dashboard
 
 
 urlpatterns = [
     path('', dashboard, name='dashboard'),
     path('categories/', categories, name='categories'),
+    path('categories/<int:category_id>', category, name='category'),
 ]
