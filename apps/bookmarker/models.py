a7a3fb2c11cce28e557b88b67d0f20c3fd917886
Artus U <artusrock@hotmail.com>
2022-10-27 09:30:51 +0100

    Bookmark model

--- apps/bookmarker/models.py
+++ apps/bookmarker/models.py
@@ -15,3 +15,20 @@ class Category(models.Model):
 
     def __str__(self):
         return self.title
+    
+    
+class Bookmark(models.Model):
+    category = models.ForeignKey(Category, related_name='bookmarks', on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    url = models.CharField(max_length=255)
+    visits = models.IntegerField(default=0)
+
+    created_by = models.ForeignKey(User, related_name='bookmarks', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-visits',)
+
+    def __str__(self):
+        return self.title
