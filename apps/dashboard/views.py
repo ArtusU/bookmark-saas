0a3fee2b74d1a99d8f3414bf3236e6d60c85844c
Artus U <artusrock@hotmail.com>
2022-11-17 11:00:16 +0000

    user settings logic

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 
 @login_required
@@ -13,4 +15,29 @@ def dashboard(request):
 
 @login_required
 def settings(request):
+    if request.method == "POST":
+        first_name = request.POST.get("first_name", "")
+        last_name = request.POST.get("last_name", "")
+        username = request.POST.get("username", "")
+        email = request.POST.get("email", "")
+
+        user = request.user
+
+        if username != request.user.username:
+            users = User.objects.filter(username=username)
+
+            if len(users):
+                messages.error(request, "The username already exists!")
+            else:
+                user.username = username
+
+        user.first_name = first_name
+        user.last_name = last_name
+        user.email = email
+        user.save()
+
+        messages.success(request, "The changes was saved!")
+
+        return redirect("dashboard")
+
     return render(request, "dashboard/settings.html")
