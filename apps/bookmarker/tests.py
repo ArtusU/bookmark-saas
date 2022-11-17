e1f0ab0dc34751bfe799e3603d16f619ff3b7862
Artus U <artusrock@hotmail.com>
2022-11-17 12:47:26 +0000

    TestBookmarkerModels

--- apps/bookmarker/tests.py
+++ apps/bookmarker/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your tests here.
+from apps.bookmarker.models import Category
+
+
+class TestBookmarkerModels(TestCase):
+    def create_category(self, title="Funny Dogs", description="New content"):
+        testuser = User.objects.create_user(username="testuser", password="12345")
+        return Category.objects.create(
+            title=title,
+            description=description,
+            created_at=timezone.now(),
+            created_by=testuser,
+        )
+
+    def test_category_creation(self):
+        c = self.create_category()
+        self.assertTrue(isinstance(c, Category))
+        self.assertEqual(c.__str__(), c.title)
