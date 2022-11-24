d757b134c8493e7332693a3ab3a4356f2367c3ba
Artus U <artusrock@hotmail.com>
2022-11-24 09:17:08 +0000

    Userprofile Model

--- apps/userprofile/models.py
+++ apps/userprofile/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Userprofile(models.Model):
+    BASIC = 'basic'
+    PRO = 'pro'
+
+    CHOICES_PLANS = (
+        (BASIC, 'Basic'),
+        (PRO, 'Pro')
+    )
+
+    user = models.OneToOneField(User, related_name='userprofile', on_delete=models.CASCADE)
+    plan = models.CharField(max_length=20, choices=CHOICES_PLANS, default=BASIC)
+    subscription = models.CharField(max_length=100, default='')
